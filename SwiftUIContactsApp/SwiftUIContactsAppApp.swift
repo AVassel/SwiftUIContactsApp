594e2d37f9528d85b660107d65c35e003690226f
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-05-30 13:44:54 +0300

    Initial Commit

---
+++ SwiftUIContactsApp/SwiftUIContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContactsAppApp.swift
+//  SwiftUIContactsApp
+//
+//  Created by Anton Vassel on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
